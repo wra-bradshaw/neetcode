7d2647a115ce430941c96faaf7534de62463a62d
William Bradshaw <will.bradshaw50@gmail.com>
2026-04-05 11:29:55 +1000

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+
+@dataclass
+class Car:
+    position: int
+    speed: int
+    
+    def __lt__(self, other) -> bool:
+        return self.position < other.position
+        
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int: 
+        if len(position) == 0:
+            return 0
+
+        cars: list[Car] = []
+        for i in range(len(position)):
+            cars.append(Car(position[i], speed[i]))
+        
+        cars_sorted = sorted(cars)
+        print(cars_sorted)
+
+        stack: list[Car] = [cars_sorted[-1]]
+        
+        for i in reversed(range(len(cars_sorted)-1)):
+            t = (target - stack[-1].position) / stack[-1].speed
+            current_car = cars_sorted[i]
+            delta_pos = stack[-1].position - current_car.position
+            delta_speed = current_car.speed - stack[-1].speed
+
+            print(i, "delta_pos: ", delta_pos, "delta_speed: ", delta_speed)
+            if delta_speed > 0:
+                catch_up_time = delta_pos / delta_speed
+                print("catch_up_time", catch_up_time)
+                if catch_up_time <= t:
+                    continue
+            stack.append(cars_sorted[i])
+        
+        return len(stack)
